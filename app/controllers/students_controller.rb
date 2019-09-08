15758c2d1b25cf7d01bc24d71fab48b3a6b7dccf
Standvpmnt <shubhang.saltech@gmail.com>
2019-09-08 23:14:12 +0530

    creating partials and sharing the flash message system

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-
+  before_action :set_student, only: [:show, :edit, :update]
   def index
     @students = Student.all
   end
@@ -12,21 +12,32 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
   end
 
   def show
-
   end
 
   def edit
+  end
 
+  def update
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully changed your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end
   end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
