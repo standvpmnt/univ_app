4142c1380c730450a2d2a2339df83acd29831c77
Standvpmnt <shubhang.saltech@gmail.com>
2019-09-09 10:12:40 +0530

    Updated to have restrictions on edit and view priviliges based on login credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  before_action :require_user
   helper_method :current_user, :logged_in?
   def current_user
     @current_user ||= Student.find(session[:student_id]) if session[:student_id]
@@ -7,4 +9,12 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:notice] = "You must be logged in to peform that action"
+      redirect_to login_path
+    end
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,8 @@
 class StudentsController < ApplicationController
+  skip_before_action :require_user, only: [:new, :create]
   before_action :set_student, only: [:show, :edit, :update]
+  before_action :require_same_student, only: [:edit, :update]
+
   def index
     @students = Student.all
   end
@@ -22,6 +25,7 @@ class StudentsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
@@ -41,6 +45,13 @@ class StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_same_student
+    if current_user != @student
+      flash[:notice] = "You can only edit your own profile"
+      redirect_to student_path(current_user)
+    end
   end
 
 end
